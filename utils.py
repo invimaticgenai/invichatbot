922df8d1812abe69035cc428399a0dd55522a6a1
invimaticgenai <149483952+invimaticgenai@users.noreply.github.com>
2023-12-12 01:41:59 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -2,8 +2,10 @@ from sentence_transformers import SentenceTransformer
 import pinecone
 import openai
 import streamlit as st
+import os
+openai_api_key = os.environ["OPENAI_API_KEY"]
 
-openai.api_key = "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"
+openai.api_key = "OPENAI_API_KEY"
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 pinecone.init(api_key='6348c0b6-eb7a-4b83-8ff9-2c2c6ab11887', environment='gcp-starter')
